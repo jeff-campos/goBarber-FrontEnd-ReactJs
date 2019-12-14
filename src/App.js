866a8b6e47dae3bc6740d663360ba7b8d56cd710
Admin <admin@Milenes-MBP.lan>
2019-12-14 18:40:31 -0300

    feat(layouts|global): create global style by type signer

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Router } from 'react-router-dom';
 import './config/ReactotronConfig';
 import Routes from './routes';
 import history from './services/history';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
