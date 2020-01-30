6d954635516b437a155ecf08f1c87446e7f1c8fb
Admin <admin@Jeff-Campos-MBP.lan>
2020-01-30 06:49:06 -0300

    feat(profile): creating page to exchange profile data

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
+        <hr />
+
+        <Input
+          type="password"
+          name="oldPassWord"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova seenha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirme sua nova seenha"
+        />
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
