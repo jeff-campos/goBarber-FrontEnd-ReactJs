433e7f9179140ca92f13e60bd57e84a6a944dfd6
Admin <admin@Jeff-Campos-MBP.lan>
2020-02-01 12:30:14 -0300

    feat(Dashboard): structure and design

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
-import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  api.get('providers');
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>01 de fevereiro</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Jeff Campos</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Livia Tenorio de Campos</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Larissa Tenorio de Campos</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>Lucas Tenorio de Campos</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
